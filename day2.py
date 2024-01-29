f0b7bce479245afaa82630ba04732928246e17db
Jaromír Cejpek <mira.cejpek@gmail.com>
2024-01-29 21:37:10 +0100

    create day2.py

---
+++ day2.py
@@ -0,0 +1,24 @@
+# adventofcode.com/2023/day2/
+
+# Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+# Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+# two1nine
+# eightwothree
+# abcone2threexyz
+# xtwone3four
+# 4nineeightseven2
+# zoneight234
+# 7pqrstsixteen
+# In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+# What is the sum of all of the calibration values?
+
+print("adventOfCode2023-day2")
+
+
+import day1
+# one, two, three, four, five, six, seven, eight, and nine
+
+# day1.getcalibrationValuesSumFromfile()
